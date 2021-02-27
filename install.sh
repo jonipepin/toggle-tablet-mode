4e1eb3dad8b959591fc6416088bccde12bc15b2b
Joni Pepin <joni@pepinonline.com>
2021-02-27 17:26:57 -0500

    check for sudo

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-sudo mkdir /opt/toggle_tablet_mode
-sudo mkdir /opt/toggle_tablet_mode/bin
-sudo mkdir /opt/toggle_tablet_mode/include
 
-sudo cp rotate_screen.sh /opt/toggle_tablet_mode/bin/
-sudo cp touchscreen.png /opt/toggle_tablet_mode/include/
-sudo cp touchscreen_blue.png /opt/toggle_tablet_mode/include/
-sudo cp rotate_tablet.png /opt/toggle_tablet_mode/include/
-sudo cp tablet_mode.desktop /usr/local/share/applications/
+if [ $EUID -ne 0 ]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+mkdir /opt/toggle_tablet_mode
+mkdir /opt/toggle_tablet_mode/bin
+mkdir /opt/toggle_tablet_mode/include
+
+cp rotate_screen.sh /opt/toggle_tablet_mode/bin/
+cp touchscreen.png /opt/toggle_tablet_mode/include/
+cp touchscreen_blue.png /opt/toggle_tablet_mode/include/
+cp rotate_tablet.png /opt/toggle_tablet_mode/include/
+cp tablet_mode.desktop /usr/local/share/applications/
 
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'tablet_mode.desktop']"
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sudo rm -Rf /opt/toggle_tablet_mode
 
-sudo rm /usr/local/share/applications/tablet_mode.desktop
+if [ $EUID -ne 0 ]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+rm -Rf /opt/toggle_tablet_mode
+
+rm /usr/local/share/applications/tablet_mode.desktop
 
 FAVORITES=$(gsettings get org.gnome.shell favorite-apps | sed "s/,*\s*'tablet_mode.desktop'//")
 
